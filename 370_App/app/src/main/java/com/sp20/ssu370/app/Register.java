8f6737aa0f5307a647cb3c61ce54049485edff2d
AustinEscalante <austinescalante@AustinEscalantes-MacBook-Pro.local>
2020-04-02 13:36:28 -0700

    Push User Data to Database

--- 370_App/app/src/main/java/com/sp20/ssu370/app/Register.java
+++ 370_App/app/src/main/java/com/sp20/ssu370/app/Register.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,17 +15,28 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import io.opencensus.tags.Tag;
 
 
 public class Register extends AppCompatActivity {
+    public static final String TAG = "TAG";
     EditText mFullName,mEmail, mPassword,mPhone;
     Button mRegisterBtn;
     TextView mLoginBtn;
     FirebaseAuth fAuth;
     ProgressBar progressBar;
+    FirebaseFirestore fStore;
+    String userID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +51,7 @@ public class Register extends AppCompatActivity {
         mLoginBtn   = findViewById(R.id.createText);
 
         fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
         progressBar = findViewById(R.id.progressBar);
 
         //Check if user already has an account, if so direct to main activity
@@ -54,8 +67,10 @@ public class Register extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 //convert text to string and trim data
-                String email = mEmail.getText().toString().trim();
-                String password = mPassword.getText().toString().trim();
+                final String email = mEmail.getText().toString().trim();
+                final String password = mPassword.getText().toString().trim();
+               final String fullName = mFullName.getText().toString();
+                final String phone = mPhone.getText().toString();
 
                 //validate data, errors
                 if(TextUtils.isEmpty(email)){
@@ -85,6 +100,18 @@ public class Register extends AppCompatActivity {
                         if(task.isSuccessful()){
                             //Display message and send user to main activity
                             Toast.makeText(Register.this,"User Created", Toast.LENGTH_SHORT).show();
+                            userID = fAuth.getCurrentUser().getUid();
+                            DocumentReference documentReference = fStore.collection("users").document(userID);
+                            Map<String, Object> user = new HashMap<>();
+                            user.put("Name",fullName);
+                            user.put("email", email);
+                            user.put("phone",phone);
+                            documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    Log.d(TAG, "onSuccess: User Profile is created for " + userID);
+                                }
+                            });
                             startActivity(new Intent(getApplicationContext(),MainActivity.class));
 
                         }else{
