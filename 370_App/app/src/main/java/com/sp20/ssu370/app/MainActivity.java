a14be95c4ac918e4bd9fc4558c37d5dc9259b5de
Christian Valencia <christianvalencia@christians-mbp.attlocal.net>
2020-04-18 16:31:39 -0700

    basic navigation bar at the bottom

--- 370_App/app/src/main/java/com/sp20/ssu370/app/MainActivity.java
+++ 370_App/app/src/main/java/com/sp20/ssu370/app/MainActivity.java
@@ -1,13 +1,17 @@
 package com.sp20.ssu370.app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
@@ -18,18 +22,57 @@ import com.google.firebase.firestore.FirebaseFirestoreException;
 import javax.annotation.Nullable;
 
 public class MainActivity extends AppCompatActivity {
+
     TextView fullName, email, phone;
     FirebaseAuth fAuth;
     FirebaseFirestore fStore;
     String userId;
 
+
+    BottomNavigationView bottomNav;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         phone = findViewById(R.id.profilePhone);
         email = findViewById(R.id.profileEmail);
         fullName = findViewById(R.id.profileName);
+
+        //Reference bottom navigation view
+        bottomNav = findViewById(R.id.bottom_navigation);
+
+        //set first fragment to home when app opens to the main activity
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+        //be able to switch in between fragments
+        bottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedFragment = null;
+
+                switch (item.getItemId()) {
+                    case R.id.nav_home:
+                        selectedFragment = new HomeFragment();
+                        break;
+
+                    case R.id.nav_profile:
+                        selectedFragment = new ProfileFragment();
+                        break;
+
+                    case R.id.nav_message:
+                        selectedFragment = new MessageFragment();
+                        break;
+
+                    case R.id.nav_map:
+                        selectedFragment = new MapFragment();
+                        break;
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+                return true;
+            }
+        });
+
 
         fAuth = FirebaseAuth.getInstance();
         fStore = FirebaseFirestore.getInstance();
@@ -53,5 +96,6 @@ public class MainActivity extends AppCompatActivity {
         startActivity(new Intent(getApplicationContext(), Login.class));
         finish();
     }
+
 }
 
